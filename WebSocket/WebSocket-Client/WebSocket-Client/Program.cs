122fec0bc13b531192046401750fdf1b70d3c176
Ricardo Salvi Machida <r.salvi10@gmail.com>
2023-07-27 16:45:03 -0300

    projeto server client usando websocketsharp

---
+++ WebSocket/WebSocket-Client/WebSocket-Client/Program.cs
@@ -0,0 +1,22 @@
+﻿using WebSocketSharp;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        using (WebSocket ws = new WebSocket("ws://127.0.0.1:7890/EchoAll"))
+        {
+            ws.Connect();
+            ws.OnMessage += Ws_OnMessage;
+            ws.Send("Hello");
+            Console.ReadKey();
+        }
+
+        static void Ws_OnMessage(object sender, MessageEventArgs e)
+        {
+            Console.WriteLine("Receive from Server:" + DateTime.Now);
+
+
+        }
+    }
+}
---
+++ WebSocket/WebSocket/WebSocket/Program.cs
@@ -0,0 +1,38 @@
+﻿using WebSocketSharp;
+using WebSocketSharp.Server;
+
+WebSocketServer wssv = new WebSocketServer("ws://127.0.0.1:7890");
+wssv.AddWebSocketService<Echo>("/Echo");
+wssv.AddWebSocketService<EchoAll>("/EchoAll");
+
+wssv.Start();
+Console.WriteLine("Server started ws/Echo");
+Console.WriteLine("Server started ws/EchoAll");
+Console.ReadLine();
+wssv.Stop();
+
+public class Echo : WebSocketBehavior
+{
+    public Echo() { }
+
+    protected override void OnMessage(MessageEventArgs e)
+    {
+        var data = string.Empty;
+        Console.WriteLine("============ Echo: =======");
+        Console.WriteLine("Received Message: " + e.Data);
+
+        Send(e.Data);
+    }
+
+}
+
+public class EchoAll : WebSocketBehavior
+{
+    protected override void OnMessage(MessageEventArgs e)
+    {
+        Console.WriteLine("========== EchoAll: ======");
+        Console.WriteLine("Received Message from EchoAll: " + e.Data);
+        Sessions.Broadcast(e.Data);
+    }
+
+}
